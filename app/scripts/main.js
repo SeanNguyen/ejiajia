32976957f65725d8dd78257547394c5b0c2e59fb
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-06-23 10:45:56 +0800

    added toggle function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+var filterMaxHeight = 1000;
+var filterMinHeight = 40;
+
+var TEXT_EXPAND = '与 4 个筛选...';
+var TEXT_COLLAPSE = '隐藏';
+
+var phoneMaxWidth = 480;
+
+function toggleFilter() {
+	if($( '.selectedItemContainer' ).height() <= filterMinHeight) {
+		$( '.selectedItemContainer' ).css({ "max-height": filterMaxHeight + 'px' });
+		$( '#expandButton' ).text(TEXT_COLLAPSE);
+	} else {
+		if($( document ).width() <= phoneMaxWidth) {
+			$( '.selectedItemContainer' ).css({ "max-height": 0 });
+		} else {
+			$( '.selectedItemContainer' ).css({ "max-height": filterMinHeight + 'px' });
+		}
+		$( '#expandButton' ).text(TEXT_EXPAND);	
+	}
+}
